f8687710ea36b0c07d5c107dcc335900dab52fa2
youssef103 <youssefmoti@yahoo.com>
2020-05-03 00:27:40 +0200

    initial commit

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Server is running"));
+app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
