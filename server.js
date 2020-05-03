796bdd67f52ef708da1a266f3c9dd39d0bc1a227
youssef103 <youssefmoti@yahoo.com>
2020-05-03 16:07:06 +0200

    run npm install

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use(`${BASE_URL}/post`, require("./routes/post"));
 
 // Server static assets in Production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/build"));
+  app.use(express.static("/client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
